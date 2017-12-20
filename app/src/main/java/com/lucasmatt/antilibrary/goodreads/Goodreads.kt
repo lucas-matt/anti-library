8664aeb6a91dbd5b92c86c5daea4af6abfeb4d36
Matthew Lucas <lucasmatthew@hotmail.com>
2017-12-20 12:45:15 +0000

    Added Fuel rest library to talk to Goodreads

--- app/src/main/java/com/lucasmatt/antilibrary/goodreads/Goodreads.kt
+++ app/src/main/java/com/lucasmatt/antilibrary/goodreads/Goodreads.kt
@@ -1,9 +1,33 @@
 package com.lucasmatt.antilibrary.goodreads
 
-import com.lucasmatt.antilibrary.R
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.httpGet
+import com.lucasmatt.antilibrary.BuildConfig
 
-class Goodreads {
+class Goodreads(private val user:String) {
 
-    val key = R.string.app_name
+    init {
+        FuelManager.instance.basePath = "https://www.goodreads.com"
+        FuelManager.instance.baseParams = listOf(Pair("key", BuildConfig.GOODREADS_API_KEY))
+    }
+
+    class UserResponse {
+        val user: User? = null
+    }
+
+    class User {
+        val id: String? = null
+        val name: String? = null
+    }
+
+    fun userInfo(): String {
+        val (_, resp, res) = "/user/show/$user".httpGet().responseString()
+        return when(resp.statusCode) {
+            200 -> {
+                res.get()
+            }
+            else -> throw RuntimeException(res.get())
+        }
+    }
 
 }

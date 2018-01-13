33ef075a187bd7463f54d3907a5a4f861561dbcc
lucas-matt <lucasmatthew@hotmail.com>
2018-01-13 20:06:22 +0000

    Cleanup activities

---
+++ app/src/main/java/com/lucasmatt/antilibrary/wallpaper/AntilibraryWallpaperService.kt
@@ -0,0 +1,11 @@
+package com.lucasmatt.antilibrary.wallpaper
+
+import android.service.wallpaper.WallpaperService
+
+class AntilibraryWallpaperService : WallpaperService() {
+
+    override fun onCreateEngine(): Engine {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
